726f6f3ab63bdb3ee2ce03130ff83351f77bfa10
Michael Davis <MichaelRDavis1991@protonmail.com>
2018-10-07 14:37:40 +0100

    Removed old FSM

--- CritterFSM/CritterFSM/Source/FSM/IState.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class IState
-{
-public:
-	virtual void OnEnter() = 0;
-	virtual void OnUpdate() = 0;
-	virtual void OnExit() = 0;
-};
--- CritterFSM/CritterFSM/Source/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "CritterGame/Game.h"
-
-int main()
-{
-	Game CritterGame;
-	CritterGame.Run();
-
-	return 0;
-}
