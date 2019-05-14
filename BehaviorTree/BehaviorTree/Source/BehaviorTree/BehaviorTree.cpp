fece20b8eabc7930938b473ab0b2c7f72a29a630
Michael Davis <MichaelRDavis1991@protonmail.com>
2019-05-14 12:04:41 +0100

    Improved Behavior Tree object.

---
+++ BehaviorTree/BehaviorTree/Source/BehaviorTree/BehaviorTree.cpp
@@ -0,0 +1,29 @@
+#include "BehaviorTree.h"
+#include "Task.h"
+
+BehaviorTree::BehaviorTree()
+	: m_blackboard(std::make_shared<Blackboard>())
+{
+
+}
+
+BehaviorTree::~BehaviorTree()
+{
+
+}
+
+void BehaviorTree::SetRootNode(const std::shared_ptr<Task>& newNode)
+{
+	m_rootNode = newNode;
+}
+
+void BehaviorTree::Tick()
+{
+	m_rootNode->Tick();
+}
+
+std::shared_ptr<Blackboard> BehaviorTree::GetBlackboard() const
+{
+	return m_blackboard;
+}
+
--- BehaviorTree/BehaviorTree/Source/BehaviorTree/BehaviorTree.h
+++ BehaviorTree/BehaviorTree/Source/BehaviorTree/BehaviorTree.h
@@ -1,15 +1,33 @@
 #pragma once
 
+#include <memory>
+
 class Task;
+class Blackboard;
 
 /** Base class for a behavior tree */
 class BehaviorTree
 {
 public:
+	/** Constructor. */
+	BehaviorTree();
+
+	/** Destructor. */
+	~BehaviorTree();
+
+	/** Set the root node of this behavior tree. */
+	void SetRootNode(const std::shared_ptr<Task>& newNode);
+
 	/** Update the behavior tree, called once per frame */
-	virtual void Tick() = 0;
+	virtual void Tick();
+
+	/** Returns the blackboard object. */
+	std::shared_ptr<Blackboard> GetBlackboard() const;
 
 protected:
 	/** The root of the behavior tree */
-	Task* m_pRoot;
+	std::shared_ptr<Task> m_rootNode;
+
+	/** The behavior trees blackboard. */
+	std::shared_ptr<Blackboard> m_blackboard;
 };
